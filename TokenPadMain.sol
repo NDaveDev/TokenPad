a5387f16a2f6d245dd9b981c9382681f749a56ca
NDaveDev <96290062+NDaveDev@users.noreply.github.com>
2022-05-24 01:40:30 +0800

    Update TokenPadMain.sol

--- TokenPadMain.sol
+++ TokenPadMain.sol
@@ -1,3 +1,106 @@
 // SPDX-License-Identifier: MIT
 
+import "./Context.sol";
+import "./Ownable.sol";
+import "./IERC20.sol";
+import "./SafeMath.sol";
+
 pragma solidity ^0.8.13;
+
+contract CoinContract is Context, IERC20, Ownable {
+    using SafeMath for uint256;
+ 
+    mapping (address => uint256) private balances;
+    mapping (address => mapping (address => uint256)) private _allowances;
+
+    mapping (address => bool) private _isExcludedFromFee;
+   
+    string private constant _name = "TokenPad";
+    string private constant _symbol = "TPAD ";
+    uint8 priva
+    te constant _decimals = 18;
+    uint256 private _tTotal = 100000000 * 10**_decimals;
+        
+    constructor () {
+        balances[_msgSender()] = _tTotal;
+        
+        emit Transfer(address(0), _msgSender(), _tTotal);
+    }
+
+    function name() public pure returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public pure returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public pure returns (uint8) {
+        return _decimals;
+    }
+
+    function totalSupply() public view override returns (uint256) {
+        return _tTotal;
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        return balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        _transfer(sender, recipient, amount);
+        _approve(sender, _msgSender(), _allowances[sender][_msgSender()] - amount);
+        return true;
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender] + addedValue);
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender] - subtractedValue);
+        return true;
+    }
+    
+    receive() external payable {}
+    
+    function _approve(address owner, address spender, uint256 amount) private {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) private {
+        require(from != address(0), "ERC20: transfer from the zero address");
+        require(to != address(0), "ERC20: transfer to the zero address");
+        require(amount > 0, "Transfer amount must be greater than zero");
+        
+        balances[from] -= amount;
+        uint256 transferAmount = amount;
+         
+        balances[to] += transferAmount;
+        emit Transfer(from, to, transferAmount);
+    }
+    
+}
